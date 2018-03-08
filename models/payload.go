96942edd166fba635eff25b5ed2bd2e25f5c35c2
yinzhenwei <yinwhm@nerthuschain.org>
2018-03-08 19:30:36 +0800

    payload 数据 model

---
+++ models/payload.go
@@ -0,0 +1,12 @@
+package models
+
+//玩家贡献数据
+
+type PayLoad struct {
+	Id int `json:"_id" bson:"_id"`
+	Asset int `json:"asset" bson:"asset"`
+	GameId string `json:"game_id" bson:"game_id"`
+	CreateTime int `json:"create_time" bson:"create_time"`
+}
+
+
